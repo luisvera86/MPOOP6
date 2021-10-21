32d510646890cf777653d427f782af7e54d0a724
Luis-AngelAng48 <>
2021-10-21 19:12:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+*@author Luis Angel Vera Reyes 
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+  /**
+  *@Param a Primer enero de la enero
+  *@Param b Segundo entero de la suma
+  *@return La suma de los enteros a mas b
+  */
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
